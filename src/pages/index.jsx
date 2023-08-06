5edbafc0059d95e05fe20aba7fe210e9390e593c
Patrick Oliveira <patrick17silva@hotmail.com>
2023-08-06 14:26:06 -0300

    Primeiro commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <main className="w-full min-h-screen bg-dark-40">
+      <Head>
+        <title>Patrick Dev</title>
+      </Head>
+    </main>
+  );
+}
